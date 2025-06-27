c3af9a9e25f6baac65fce8708bdd850c9d804ced
谎言 <1772943791@qq.com>
2025-06-28 01:50:49 +0800

    feat：element UI没有使用，现在可以使用了

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import { createPinia } from 'pinia'
 import '@/assets/reset.css'
 import App from './App.vue'
 import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus' // 加这一行
 import router from './router'
 import VChild from '@/pages/lifeCycle/VChild.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
-app.component('VChild',VChild) // 全局注册后，可以在其他组件中直接使用，不需要引入。但是不能按需加载。
+app.component('VChild', VChild)
+app.use(ElementPlus) // 加这一行
 app.use(createPinia())
-app.use(router)
 app.use(router)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
